14e607e09de41e3c20603c7b2a84fd36dc174e51
erocham <erocham@ucenfotec.ac.cr>
2020-11-13 23:28:02 -0600

    Elian

---
+++ src/DynamoDbDemo/Entities/DVD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace DynamoDbDemo.Entities
+{
+    [DynamoDBTable("DVD")]
+    public class DVD
+    {
+        [DynamoDBHashKey] // Básicamente es un primary key
+        public string Title { get; set; }
+        [DynamoDBRangeKey] // Complementary key (?) 
+        public int ReleaseYear { get; set; }
+        [DynamoDBProperty]
+        public List<string> ActorNames { get; set; }
+
+        public string Director { get; set; }
+
+        public string Producer { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format(@"{0} – {1} Actors: {2}", Title, ReleaseYear, string.Join(", ", ActorNames.ToArray()));
+        }
+
+    }
+
+
+}
